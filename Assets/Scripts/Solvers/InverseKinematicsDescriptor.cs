a5beb22ef8dca26d221657c999f8ae7256361de8
Rémi GINER <r.giner@student.isartdigital.com>
2024-03-15 12:03:14 +0100

    [ccdik] Added CCDIK with constraints

--- Assets/Scripts/Solvers/InverseKinematicsDescriptor.cs
+++ Assets/Scripts/Solvers/InverseKinematicsDescriptor.cs
@@ -5,9 +5,35 @@ using UnityEngine;
 [System.Serializable]
 public class Joint
 {
+    Joint() { }
+
+    public Joint(Transform _transform)
+    {
+        transform = _transform;
+    }
+
     public Transform transform;
 
     public Vector3 Position { get => transform.position; set => transform.position = value; }
+    public Vector3 LocalPosition { get => transform.localPosition; set => transform.localPosition = value; }
+    public Quaternion Rotation { get => transform.rotation; set => transform.rotation = value; }
+    public Quaternion LocalRotation { get => transform.localRotation; set => transform.localRotation = value; }
+    public bool UseConstraint => useConstraint;
+    public Vector3 ConstraintAxis => constraintAxis;
+    public float ConstraintMinAngle => constraintMinAngle;
+    public float ConstraintMaxAngle => constraintMaxAngle;
+
+    [SerializeField]
+    private bool useConstraint;
+
+    [SerializeField]
+    private Vector3 constraintAxis = Vector3.forward;
+
+    [SerializeField]
+    private float constraintMinAngle = -180f;
+
+    [SerializeField]
+    private float constraintMaxAngle = 180f;
 }
 
 public class InverseKinematicsDescriptor : ScriptableObject
