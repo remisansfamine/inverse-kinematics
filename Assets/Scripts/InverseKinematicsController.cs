b54cb2a6fe73ee922ac8c63d92bad988a4e0357a
r.giner <remi.giner@hotmail.fr>
2024-03-15 12:03:14 +0100

    [ccdik] Added CCDIK with constraints

--- Assets/Scripts/InverseKinematicsController.cs
+++ Assets/Scripts/InverseKinematicsController.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.Port;
 
 public class InverseKinematicsController : MonoBehaviour
 {
@@ -8,16 +12,40 @@ public class InverseKinematicsController : MonoBehaviour
 
     [SerializeField] private List<Joint> joints = new List<Joint>();
 
+    [SerializeField] private Transform baseBone = null;
+
+    [SerializeField] private Transform effector = null;
+
     [SerializeField] private Transform goal = null;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnValidate()
     {
-        
+        List<Transform> bonesTransforms = new List<Transform>();
+        Transform current = effector;
+
+        while (current != null && current != baseBone)
+        {
+            bonesTransforms.Add(current);
+            current = current.parent;
+        }
+
+        int deltaSize = joints.Count - bonesTransforms.Count;
+        if (deltaSize < 0)
+        {
+            joints.AddRange(bonesTransforms.GetRange(0, -deltaSize).Select(x => new Joint(x)));
+        }
+        else
+        {
+            joints.RemoveRange(bonesTransforms.Count, deltaSize);
+        }
+
+        for (int i = 0; i < joints.Count; i++)
+        {
+            joints[i].transform = bonesTransforms[i];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (goal)
             Solver?.UpdateJoints(ref joints, goal.position);
